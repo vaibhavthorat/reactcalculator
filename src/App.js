dfe64c3f11ed3e953a08896c937002fcf353915c
vaibhav thorat <vaibhavthorat115@gmail.com>
2023-01-03 20:36:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react'
+import Calculator from './Calculator'
+
+
+export default function App() {
+  const [flag,setFlag]=useState(false);
+  return <>
+
+{flag?<Calculator/>:null}
+  
+ 
+<button style={{backgroundColor:'aquamarine', position:'absolute',left:'49%', top:'5%',  border:'3px solid black',width:'auto',height:'40px'}} onClick={()=>setFlag(!flag)}>Open Calculator</button>
+  </>
 }
-
-export default App;
